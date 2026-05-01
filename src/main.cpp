13623180b6c7ce32f3d217bf3c61f5d2869540a5
m <chrg>
2026-05-01 19:27:20 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@ int main() {
     std::cout << "$ ";
     std::string x;
     std::cin>> x;
+    if(x == "exit"){
+      running = false;
+      continue;
+    }
     std::cout<<x<<": command not found"<<std::endl;
   }
   
