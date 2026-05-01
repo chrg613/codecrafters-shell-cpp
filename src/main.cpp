09f240c42260c3b2efb415654becc31a14b5b1ce
m <chrg>
2026-05-01 19:42:26 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -11,18 +11,30 @@ int main() {
     std::cout << "$ ";
     std::string command;
     std::cin>> command;
-    if(command == "exit"){
-      running = false;
-      continue;
+    std::vector<std::string> commands={"exit","echo","type"};
+    if (std::find(commands.begin(), commands.end(), command) != commands.end()) {
+      std::cout<<command<<": command not found"<<std::endl;
     }
-    else if(command=="echo"){
+    else if(command == commands[0]){
+      running = false;
+    }
+    else if(command==commands[1]){
       std::string echo_line;
       std::cin.ignore();
       std::getline(std::cin,echo_line);
       std::cout<<echo_line<<std::endl;
-      continue;
     }
-    std::cout<<command<<": command not found"<<std::endl;
+    else if(command==commands[2]){
+      std::string command_type;
+      std::cin>>command_type;
+      if (std::find(commands.begin(), commands.end(), command_type) != commands.end()) {
+        std::cout<<command_type<<" is a shell builtin"<<std::endl;
+      }
+      else{
+        std::cout<<command_type<<": not found"<<std::endl;
+      }
+    }
+    
   }
   
   
