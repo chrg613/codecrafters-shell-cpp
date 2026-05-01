e48e7876a9111ebe1c2190e8d193c9f9cf992b1c
m <chrg>
2026-05-01 19:54:16 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-std::vector<std::string> commands={"exit","echo","type"};
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,25 +11,26 @@ int main() {
     std::cout << "$ ";
     std::string command;
     std::cin>> command;
-    
-    if (std::find(commands.begin(), commands.end(), command) != commands.end()) {
+    std::vector<std::string> commands={"exit","echo","type"};
+    if (std::find(commands.begin(), commands.end(), command) == commands.end()) {
       std::cout<<command<<": command not found"<<std::endl;
+      continue;
     }
-    else if(command == commands[0]){
+    if(command == commands[0]){
       running = false;
       continue;
     }
-    else if(command==commands[1]){
+    if(command==commands[1]){
       std::string echo_line;
       std::cin.ignore();
       std::getline(std::cin,echo_line);
       std::cout<<echo_line<<std::endl;
       continue;
     }
-    else if(command==commands[2]){
+    if(command==commands[2]){
       std::string command_type;
       std::cin>>command_type;
-      if (std::find(commands.begin(), commands.end(), command_type) != commands.end()) {
+      if (std::find(commands.begin(), commands.end(), command_type) == commands.end()) {
         std::cout<<command_type<<": not found"<<std::endl;
       }
       else{
